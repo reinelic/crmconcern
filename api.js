30ca268ab6db203874ab97b18e9ca3be4c9f6cef
reineAlice <reinelic13@gmail.com>
2023-02-28 19:45:04 +0200

    remove google api

--- api.js
+++ api.js
@@ -1,13 +1,18 @@
-var request=require("request");
+var axios = require('axios')
 
-request('http://www.google.com',function(error,response,body){
-    if(!error)
-    {
-        console.log(error + "SOMETHING WRENT WRONG");
-    }
-    else if(response.statusCode == 200)
-    {
-        console.log(body)
-    }
-    
-})
+// request('http://www.google.com', function (error, response, body) {
+//   if (!error) {
+//     console.log(error + 'SOMETHING WRENT WRONG')
+//   } else if (response.statusCode == 200) {
+//     console.log(body)
+//   }
+// })
+
+// axios
+//   .get(' http://www.google.com')
+//   .then(function (error, response, body) {
+//     console.log(response)
+//   })
+//   .catch(function (error) {
+//     console.log(error)
+//   })
--- routes/index.js
+++ routes/index.js
@@ -1,179 +1,161 @@
-var express=require("express");
-var router=express.Router();
-var User = require("../models/user");
-var passport = require("passport");
-
+var express = require('express')
+var router = express.Router()
+var User = require('../models/user')
+var passport = require('passport')
 
 // CALENDAR API
 //============
 
+const fs = require('fs')
+const readline = require('readline')
+const { google } = require('googleapis')
 
+// // If modifying these scopes, delete token.json.
+// const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+// const TOKEN_PATH = 'token.json';
 
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
+// // Load client secrets from a local file.
+// fs.readFile('credentials.json', (err, content) => {
+//   if (err) return console.log('Error loading client secret file:', err);
+//   // Authorize a client with credentials, then call the Google Calendar API.
+//   authorize(JSON.parse(content), listEvents);
+// });
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-const TOKEN_PATH = 'token.json';
+// /**
+//  * Create an OAuth2 client with the given credentials, and then execute the
+//  * given callback function.
+//  * @param {Object} credentials The authorization client credentials.
+//  * @param {function} callback The callback to call with the authorized client.
+//  */
+// function authorize(credentials, callback) {
+//   const {client_secret, client_id, redirect_uris} = credentials.installed;
+//   const oAuth2Client = new google.auth.OAuth2(
+//       client_id, client_secret, redirect_uris[0]);
 
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
-});
+//   // Check if we have previously stored a token.
+//   fs.readFile(TOKEN_PATH, (err, token) => {
+//     if (err) return getAccessToken(oAuth2Client, callback);
+//     oAuth2Client.setCredentials(JSON.parse(token));
+//     callback(oAuth2Client);
+//   });
+// }
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+// /**
+//  * Get and store new token after prompting for user authorization, and then
+//  * execute the given callback with the authorized OAuth2 client.
+//  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+//  * @param {getEventsCallback} callback The callback for the authorized client.
+//  */
+// function getAccessToken(oAuth2Client, callback) {
+//   const authUrl = oAuth2Client.generateAuthUrl({
+//     access_type: 'offline',
+//     scope: SCOPES,
+//   });
+//   console.log('Authorize this app by visiting this url:', authUrl);
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+//   rl.question('Enter the code from that page here: ', (code) => {
+//     rl.close();
+//     oAuth2Client.getToken(code, (err, token) => {
+//       if (err) return console.error('Error retrieving access token', err);
+//       oAuth2Client.setCredentials(token);
+//       // Store the token to disk for later program executions
+//       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+//         if (err) console.error(err);
+//         console.log('Token stored to', TOKEN_PATH);
+//       });
+//       callback(oAuth2Client);
+//     });
+//   });
+// }
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
+// /**
+//  * Lists the next 10 events on the user's primary calendar.
+//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+//  */
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
+// var schedules = "";
 
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
- 
- 
-var schedules = "";
- 
-function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    
-    schedules=events;
-    console.log(typeof schedules);
-    
-    if (events.length) {
-         
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
-}
-// ROUTES
-//++++++++
+// function listEvents(auth) {
+//   const calendar = google.calendar({version: 'v3', auth});
+//   calendar.events.list({
+//     calendarId: 'primary',
+//     timeMin: (new Date()).toISOString(),
+//     maxResults: 10,
+//     singleEvents: true,
+//     orderBy: 'startTime',
+//   }, (err, res) => {
+//     if (err) return console.log('The API returned an error: ' + err);
+//     const events = res.data.items;
 
+//     schedules=events;
+//     console.log(typeof schedules);
 
+//     if (events.length) {
 
-router.get("/",function(req,res){
-    
-    res.render("home",{currentUser:req.user, events:schedules});
-})
+//       console.log('Upcoming 10 events:');
+//       events.map((event, i) => {
+//         const start = event.start.dateTime || event.start.date;
+//         console.log(`${start} - ${event.summary}`);
+//       });
+//     } else {
+//       console.log('No upcoming events found.');
+//     }
+//   });
+// }
+// // ROUTES
+// //++++++++
 
-//+++++++++++++++   
+// router.get("/",function(req,res){
+
+//     res.render("home",{currentUser:req.user, events:schedules});
+// })
+
+//+++++++++++++++
 // AUTH ROUTES
 
 // REGISTER ROUTES
-router.get("/register",function(req,res){
-    
-    res.render("register",{currentUser:req.user});
-});
+router.get('/register', function (req, res) {
+  res.render('register', { currentUser: req.user })
+})
 
-router.post("/register",function(req,res){
-    var newUser= new User({username:req.body.username,email:req.body.email}); 
-    User.register(newUser,req.body.password,function(err,user){
-        if(err)
-        {
-            console.log(err);
-            return res.render("register");
-        }
-        else
-        {
-            passport.authenticate("local")(req,res,function(){
-                res.redirect("/plaint")
-            })
-        }
-        
-    })
+router.post('/register', function (req, res) {
+  var newUser = new User({ username: req.body.username, email: req.body.email })
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err)
+      return res.render('register')
+    } else {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/plaint')
+      })
+    }
+  })
 })
 
 // LOGIN ROUTES
 
-router.get("/login",function(req,res){
-    res.render("login",{currentUser:req.user});
-});
+router.get('/login', function (req, res) {
+  res.render('login', { currentUser: req.user })
+})
 
-router.post("/login",passport.authenticate("local",
-{ 
-   
-    successRedirect:"/plaint",
-    failureRedirect:"/login"
-    
-}),function(req,res){
- 
-});
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/plaint',
+    failureRedirect: '/login',
+  }),
+  function (req, res) {}
+)
 
 // LOGOUT
 
-router.get("/logout",function(req,res){
-    req.logout();
-    req.flash("success","Vous venez de vous déconnecter du plateforme");
-    res.redirect("/");
+router.get('/logout', function (req, res) {
+  req.logout()
+  req.flash('success', 'Vous venez de vous déconnecter du plateforme')
+  res.redirect('/')
 })
 
-
-
-
-
-
-module.exports=router;
-
+module.exports = router
